9b1de90d6990bf33a89ea718c0bd1470668d4774
vt-d <cbayush6@gmail.com>
2023-09-24 14:05:38 +0530

    first commit

---
+++ backend/target/debug/deps/chatty_backend-ac74a09828371b05.d
@@ -0,0 +1,5 @@
+/home/vt/dev/chatapp/backend/target/debug/deps/chatty_backend-ac74a09828371b05.rmeta: src/main.rs
+
+/home/vt/dev/chatapp/backend/target/debug/deps/chatty_backend-ac74a09828371b05.d: src/main.rs
+
+src/main.rs:
---
+++ backend/target/debug/deps/chatty_backend-fd4379c8d5c9645e.d
@@ -0,0 +1,5 @@
+/home/vt/dev/chatapp/backend/target/debug/deps/chatty_backend-fd4379c8d5c9645e.rmeta: src/main.rs
+
+/home/vt/dev/chatapp/backend/target/debug/deps/chatty_backend-fd4379c8d5c9645e.d: src/main.rs
+
+src/main.rs:
